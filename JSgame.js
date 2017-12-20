196e81c752fff609d5552334ca44b3455eb7fb9a
Boony Guzman <josueguzman89@gmail.com>
2017-12-20 17:46:57 -0500

    style fixed again

--- JSgame.js
+++ JSgame.js
@@ -1,3 +1,5 @@
+// We need to use jQuery for the following:
+
 var player1 = prompt("Player One: Enter Your Name , you will be Blue");
 var player1Color = 'rgb(86, 151, 255)';
 
@@ -7,18 +9,22 @@ var player2Color = 'rgb(237, 45, 73)';
 var game_on = true;
 var table = $('table tr');
 
-
-// Change color of a button
+function reportWin(rowNum,colNum) {
+  console.log("You won starting at this row,col");
+  console.log(rowNum);
+  console.log(colNum);
+}
+// Change the color of a button
 function changeColor(rowIndex,colIndex,color) {
   return table.eq(rowIndex).find('td').eq(colIndex).find('button').css('background-color',color);
 }
 
-// Report Back to current color
+// Report Back to current color of a button
 function returnColor(rowIndex,colIndex) {
   return table.eq(rowIndex).find('td').eq(colIndex).find('button').css('background-color');
 }
 
-// Take column index, returns the bottom row that is still gray
+// Take in column index, returns the bottom row that is still gray
 function checkBottom(colIndex) {
   var colorReport = returnColor(5,colIndex);
   for (var row = 5; row > -1; row--) {
